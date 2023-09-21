c2f3ccd9fb25222e2cc69cbc2497c5430055acc8
SUBINJANG <molly1998@naver.com>
2023-09-21 17:54:56 +0900

    230921 splash screen, AppBar, BottomNavigationBar 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:fur_ever_love/settings/landing_page.dart';
+// import 'package:fur_ever_love/mainpages/main_home.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() { // 앱 실행
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyApp extends StatelessWidget { // 상태 변경 없는 위젯. 한번 UI가 그려지면 그대로 있음. splash screen
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return GetMaterialApp(
+      home: LandingPage(),
+      debugShowCheckedModeBanner: false, // 화면에 debug 표시 안보이게 함
+      theme: ThemeData(fontFamily: 'Dapretty'), //앱 전체 폰트 지정
+      themeMode: ThemeMode.system,
     );
   }
 }
+//   @override
+//   Widget build(BuildContext context) { // UI 만드는 부분
+//     return MaterialApp( // MaterialApp Design(구글 기본 디자인) 사용해서 앱 만듦
+//       title: 'Fur Ever Love',
+//       debugShowCheckedModeBanner: false, // 화면에 debug 표시 안보이게 함
+//       theme: ThemeData(fontFamily: 'Dapretty'), //앱 전체 폰트 지정
+//       themeMode: ThemeMode.system,
+//       home: MyHomePage(),
+//       routes: {
+//         '/index': (context) => IndexScreen(),
+//       },
+//       initialRoute: '/index',
+//
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: LandingPage(),
+//     );
+//   }
+// }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("FUR-EVER LOVE"),
+//         backgroundColor: Colors.white54,
+//         leading: Padding(
+//           padding: const EdgeInsets.all(9.0),
+//           child: Image.asset(
+//             'assets/images/mango.png', // 이미지 파일의 경로
+//             width: 20, // 이미지의 가로 너비 조절
+//             height: 20, // 이미지의 세로 높이 조절
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// FontAwesomeIcons
+// class MyWidget extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return IconButton(
+//       // Use the FaIcon Widget + FontAwesomeIcons class for the IconData
+//         icon: FaIcon(FontAwesomeIcons.gamepad),
+//         onPressed: () { print("Pressed"); }
+//     );
+//   }
+// }
